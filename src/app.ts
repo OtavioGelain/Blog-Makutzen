a111b28e75fb8b8841f8906ad90eda755d47a4a0
OtavioGelain <otavioluizgelain@gmail.com>
2026-03-26 19:44:45 -0300

    adding user.controller

--- src/app.ts
+++ src/app.ts
@@ -3,3 +3,4 @@ import express from 'express'
 export const app = express()
 
 app.use(express.json())
+
---
+++ src/controllers/user.controller.ts
@@ -0,0 +1,97 @@
+import { UserService } from "../services/user.service";
+import { Request, Response } from "express";
+
+export class UserController{
+    static async createUser(req: Request, res: Response): Promise<Response>{
+        try{
+            const user = await UserService.createUser(req.body)
+            return res.status(200).json({message: "User created", user})
+        }catch(error){
+            if(error instanceof Error){
+                return res.status(400).json({message: error.message})
+            }
+            return res.status(500).json({message: "Internal server error"})
+        }
+    }
+    static async showUsers(req: Request, res: Response): Promise<Response>{
+        try{
+            const user = await UserService.showUser()
+            return res.status(200).json(user)
+        }catch(error){
+            return res.status(500).json({message: "Internal server error"})
+        }
+    }
+    static async showUserById(req: Request, res: Response): Promise<Response>{
+        try{
+            const id = Number(req.params.id)
+            const user = await UserService.showUserById(id)
+            if(!id){
+                return res.status(404).json({message: "Internal server error"})
+            }
+            return res.status(200).json(user)
+        }catch(error){
+            if(error instanceof Error){
+                return res.status(400).json({message: error.message})
+            }
+            return res.status(500).json({message: "Internal server error"})
+        }
+    }
+    static async showUserByUsername(req: Request, res: Response): Promise<Response>{
+        try{
+            const user = await UserService.showUserByUsername(req.body)
+            if(!user){
+                return res.status(404).json({message: "Username not found"})
+            }
+            return res.status(200).json(user)
+        }catch(error){
+            if(error instanceof Error){
+                return res.status(400).json({message: error.message})
+            }
+            return res.status(500).json({message: "Internal server error"})
+        }
+    }
+    static async showUserByName(req: Request, res: Response): Promise<Response>{
+        try{
+            const user = await UserService.showUserByName(req.body)
+            if(!user){
+                return res.status(404).json({message: "Name not found"})
+            }
+            return res.status(200).json(user)
+        }catch(error){
+            if(error instanceof Error){
+                return res.status(400).json({message: error.message})
+            }
+            return res.status(500).json({message: "Internal server error"})
+        }
+    }
+    static async updateUser(req: Request, res: Response): Promise<Response>{
+        try{
+            const id = Number(req.params.id)
+            if(!id){
+                return res.status(404).json({message: "User not found"})
+            }
+            const user = await UserService.updateUser(id, req.body)
+            return res.status(200).json(user)
+        }catch(error){
+            if(error instanceof Error){
+                return res.status(400).json({message: error.message})
+            }
+            return res.status(500).json({message: "Internal server error"})
+        }
+    }
+    static async deleteUser(req: Request, res: Response): Promise<Response>{
+        try{
+            const id = Number(req.params.id)
+            if(!id){
+                return res.status(404).json({message: "User not found"})
+            }
+            const user = await UserService.deleteUser(id)
+            return res.status(200).json(user)
+        }catch(error){
+            if(error instanceof Error){
+                return res.status(400).json({message: error.message})
+            }
+            return res.status(500).json({message: "Internal server error"})
+        }
+    }
+}
