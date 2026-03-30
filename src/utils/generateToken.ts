b1601074dce378e6cc2bff8bdca1dd05f5b1ddbe
OtavioGelain <otavioluizgelain@gmail.com>
2026-03-30 14:18:21 -0300

    fixing errors

--- src/utils/generateToken.ts
+++ src/utils/generateToken.ts
@@ -4,6 +4,7 @@ import 'dotenv/config'
 
 export function generateToken(user: User): string {
     const secretKey = process.env.JWT_KEY as string
+    
     const token = jwt.sign({id: user.id, username: user.username}, secretKey, {expiresIn: '1h'})
     return token
 }
