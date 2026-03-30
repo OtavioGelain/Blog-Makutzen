51da0053180586bcb6588f6136f25fae7073d108
OtavioGelain <otavioluizgelain@gmail.com>
2026-03-30 20:11:24 -0300

    adding comment controller

---
+++ src/controllers/comment.controller.ts
@@ -0,0 +1,60 @@
+import { Request, Response } from "express";
+import { CommentService } from "../services/comment.service";
+
+export class CommentController{
+    static async createComment(req: Request, res: Response): Promise<Response>{
+        try{
+            const comment = await CommentService.createComment(req.body)
+            return res.status(200).json(comment)
+        }catch(error){
+            if(error instanceof Error){
+                return res.status(400).json({message: error.message})
+            }
+            return res.status(500).json({message: "Internal server error"})
+        }
+    }
+    static async showComments(req: Request, res: Response): Promise<Response>{
+        try{
+            const page = Number(req.query.page) || 1
+            const limit = Number(req.query.limit) || 10
+
+            const comments = await CommentService.showComments(page, limit)
+            return res.status(200).json(comments)
+        }catch(error){
+            if(error instanceof Error){
+                return res.status(400).json({message: error.message})
+            }
+            return res.status(500).json({message: "Internal server error"})
+        }
+    }
+    static async updateComment(req: Request, res: Response):Promise<Response>{
+        try{
+            const id = Number(req.params.id)
+            const comment = await CommentService.updateComment(id, req.body)
+            if(!id){
+                return res.status(404).json({message: "Comment not found"})
+            }
+            return res.status(200).json(comment)
+        }catch(error){
+            if(error instanceof Error){
+                return res.status(400).json({message: error.message})
+            }
+            return res.status(500).json({message: "Internal server error"})
+        }
+    }
+    static async deleteComment(req: Request, res: Response): Promise<Response>{
+        try{
+            const id = Number(req.params.id)
+            const comment = await CommentService.deleteComment(id)
+            if(!id){
+                return res.status(404).json({message: "Comment not found"})
+            }
+            return res.status(200).json(comment)
+        }catch(error){
+            if(error instanceof Error){
+                return res.status(400).json({message: error.message})
+            }
+            return res.status(500).json({message: "Internal server error"})
+        }
+    }
+}
